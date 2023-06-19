18bb687e1847c0fb90dcfc46e892f91712b97669
Ismail K <ismailkoormath037@gmail.com>
2023-06-19 17:18:20 +0530

    add utils file

--- lib/constants/error_handling.dart
+++ lib/constants/error_handling.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:amazon_clone/constants/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -5,12 +8,18 @@ void httpErrorHandle({
   required http.Response response,
   required BuildContext context,
   required VoidCallback onSuccess,
-}){
-  switch(response.statusCode){
+}) {
+  switch (response.statusCode) {
     case 200:
-    onSuccess();
-    break;
+      onSuccess();
+      break;
     case 400:
-  
+      showSnackBar(context, jsonDecode(response.body)['msg']);
+      break;
+    case 500:
+      showSnackBar(context, jsonDecode(response.body)['error']);
+      break;
+    default:
+      showSnackBar(context, response.body);
   }
 }
--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -1,11 +1,15 @@
+import 'package:amazon_clone/constants/error_handling.dart';
 import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/constants/utils.dart';
 import 'package:amazon_clone/models/user.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService {
   // sign up user
 
   void signUpUser({
+    required BuildContext context,
     required String name,
     required String email,
     required String password,
@@ -28,6 +32,12 @@ class AuthService {
           'Content-Type': 'application/json; charset=UTF-8',
         },
       );
-    } catch (e) {}
+      httpErrorHandle(response: res, context: context, onSuccess: () {
+        showSnackBar(
+            context, 'Account created! Login with the same credentials');
+      },);
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
   }
 }
